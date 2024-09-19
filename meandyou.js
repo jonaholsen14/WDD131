d69dc1b2e8fa52fb22fdec11fce0c4a7892d6004
jonaholsen14 <jonaho@byui.edu>
2024-09-19 14:44:51 -0600

    starting new repo

---
+++ meandyou.js
@@ -0,0 +1,7 @@
+const MYAPPLES = 4;
+const FRIENDAPPLES = 2;
+let total = MYAPPLES + FRIENDAPPLES;
+
+document.getElementById("myAppleElement").textContent = MYAPPLES;
+document.getElementById("friendAppleElement").textContent = FRIENDAPPLES;
+document.getElementById("totalAppleElement").textContent = total;
