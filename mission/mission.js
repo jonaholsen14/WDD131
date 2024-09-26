aac219450f2ae22accb6df637655f7625ab9b7cb
jonaholsen14 <jonaho@byui.edu>
2024-09-26 16:02:02 -0600

    added darkmode

---
+++ mission/mission.js
@@ -0,0 +1,19 @@
+var selecter = document.getElementById("theme");
+var logoImage = document.getElementById('byui-logo');
+
+selecter.addEventListener("change", (event) => {
+    changeTheme();
+});
+
+function changeTheme() {
+    if (selecter.value === "dark") {
+        document.getElementById("body").className = "dark";
+        logoImage.src = '../images/byui-logo_white.png';
+        console.log("dark");
+    }
+    else {
+        document.getElementById("body").className.remove = "dark";
+        console.log("light");
+    }
+    console.log(selecter.value);
+}
